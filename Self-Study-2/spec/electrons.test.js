775cd2cd410e2c44a4c44edd7852a5eb2538ff02
Kevsi <kevsertuerkmen@gmail.com>
2026-06-11 11:15:54 +0200

    SS2: TDD Step 1 - failing test for dice value 5

--- Self-Study-2/spec/electrons.test.js
+++ Self-Study-2/spec/electrons.test.js
@@ -4,4 +4,8 @@ describe('electronsAroundTheCores', () => {
     test('Wuerfel mit Wert 3 ergibt 2', () => {
         expect(electronsAroundTheCores([3])).toBe(2);
     });
+
+    test('Wuerfel mit Wert 5 ergibt 4', () => {
+        expect(electronsAroundTheCores([5])).toBe(4);
+    });
 });
