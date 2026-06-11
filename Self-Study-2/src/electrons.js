91c244a3d8b50d2cbd569ffe560126c2c0b649f4
Kevsi <kevsertuerkmen@gmail.com>
2026-06-11 11:20:12 +0200

    SS2: TDD Step 2 - implement core logic for dice values 3 and 5

--- Self-Study-2/src/electrons.js
+++ Self-Study-2/src/electrons.js
@@ -1,5 +1,10 @@
 function electronsAroundTheCores(dice) {
-    return 2;
+    let total = 0;
+    for (const value of dice) {
+        if (value === 3) total += 2;
+        if (value === 5) total += 4;
+    }
+    return total;
 }
 
 module.exports = { electronsAroundTheCores };
