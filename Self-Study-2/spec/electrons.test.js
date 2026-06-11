b0677be6270dea68e9c5a5a0a397fa906323afd4
Kevsi <kevsertuerkmen@gmail.com>
2026-06-11 11:27:41 +0200

    SS2: TDD Step 1+2 - tests and passing implementation for value 0 and multi-dice sums

--- Self-Study-2/spec/electrons.test.js
+++ Self-Study-2/spec/electrons.test.js
@@ -8,4 +8,18 @@ describe('electronsAroundTheCores', () => {
     test('Wuerfel mit Wert 5 ergibt 4', () => {
         expect(electronsAroundTheCores([5])).toBe(4);
     });
+
+    test('Wuerfel ohne Elektronen ergibt 0', () => {
+        expect(electronsAroundTheCores([1])).toBe(0);
+        expect(electronsAroundTheCores([2])).toBe(0);
+        expect(electronsAroundTheCores([4])).toBe(0);
+        expect(electronsAroundTheCores([6])).toBe(0);
+    });
+
+    test('mehrere Wuerfel werden korrekt summiert', () => {
+        expect(electronsAroundTheCores([1, 2, 3, 4, 5])).toBe(6);
+        expect(electronsAroundTheCores([2, 2, 3, 3])).toBe(4);
+        expect(electronsAroundTheCores([6, 6, 4, 4, 1, 3])).toBe(2);
+        expect(electronsAroundTheCores([3, 5, 3, 5, 4, 2])).toBe(12);
+    });
 });
