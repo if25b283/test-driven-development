ace7ab7e03df48e5300c2823adc08723cd6437e5
Kevsi <kevsertuerkmen@gmail.com>
2026-06-11 10:38:51 +0200

    SS1: TDD Step 2 - implement findSpaceship logic for larger maps, add reflection template

--- Self-Study-1/src/alf.js
+++ Self-Study-1/src/alf.js
@@ -1,4 +1,16 @@
 function findSpaceship(map) {
+    const rows = map.split('\n');
+    const height = rows.length;
+
+    for (let rowIndex = 0; rowIndex < height; rowIndex++) {
+        const colIndex = rows[rowIndex].indexOf('X');
+        if (colIndex !== -1) {
+            const x = colIndex;
+            const y = height - 1 - rowIndex;
+            return [x, y];
+        }
+    }
+
     return [0, 0];
 }
 
